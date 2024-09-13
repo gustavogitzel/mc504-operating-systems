aa6021c09274509ba8c7562567db9014927ba263
Gustavo Gitzel <gustavo.gitzel@ifood.com.br>
2024-09-13 20:24:47 -0300

    improve bootloader

--- exp3/bootloader.asm
+++ exp3/bootloader.asm
@@ -1,41 +1,24 @@
-; bootloader.asm
-BITS 16
-ORG 0x7C00
+BITS 16                  ; indica que o código será montado em modo de 16 bits
+ORG 0x7C00               ; define a origem com o endereço onde o BIOS carrega o setor de boot na memória
 
 start:
-    ; Limpa a tela
-    mov ax, 0x0003
-    int 0x10
+    mov ax, 0x0003       ; coloca no modo vídeo 3 (texto 80x25, 16 cores)
+    int 0x10             ; chama a interrupção de BIOS para configurar o modo de vídeo
 
-    ; Configura o modo de vídeo
-    mov ah, 0x0E
-    mov al, 'H'
-    int 0x10
-    mov al, 'e'
-    int 0x10
-    mov al, 'l'
-    int 0x10
-    mov al, 'l'
-    int 0x10
-    mov al, 'o'
-    int 0x10
-    mov al, ','
-    int 0x10
-    mov al, ' '
-    int 0x10
-    mov al, 'B'
-    int 0x10
-    mov al, 'o'
-    int 0x10
-    mov al, 'o'
-    int 0x10
-    mov al, 't'
-    int 0x10
-    mov al, '!'
-    int 0x10
+    mov si, msg          ; coloca o endereço da string msg em no si
+
+print_string:
+    lodsb                ; pega o próximo byte da string e coloca no al
+    cmp al, 0            ; verifica se chegou no fim da string
+    je hang              ; se for fim da string (terminador nulo) pula pro loop
+    mov ah, 0x0E         ; configura o AH com a função de imprimir o caractere
+    int 0x10             ; chama a interrupção de BIOS para imprimir o caractere que está em al
+    jmp print_string     ; repete o processo pro próximo caractere
 
 hang:
-    jmp hang
+    jmp hang             ; entra no loop
 
-times 510-($-$$) db 0
-dw 0xAA55
+msg db 'Hello, Boot!', 0 ; definição da string que será printada com final nulo (0)
+
+times 510-($-$$) db 0    ; preenche o restante do setor de 512 bytes com zeros
+dw 0xAA55                ; assinatura do boot
